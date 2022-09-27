63e06c2864ea1246575832ef2fa7aa45904e4c37
hashirkz <hashirxkhan1@gmail.com>
2022-09-27 13:44:56 -0400

    fixed merge issues

--- twind.config.ts
+++ twind.config.ts
@@ -2,27 +2,11 @@ import { Options } from "$fresh/plugins/twind.ts";
 import * as colors from "twind/colors";
 
 export default {
-  selfURL: import.meta.url,
-<<<<<<< HEAD
-  darkMode: "class",
-  extend: {
-    colors: {
-      black: colors.black,
-      gray: colors.gray,
-      white: colors.white,
-      transparent: "transparent",
-      red: colors.red,
-    },
-    spacing: {
-      18: "4.5rem",
-    },
-=======
-  theme: {
+    selfURL: import.meta.url,
+    darkMode: "class",
     extend: {
       colors: {
         black: colors.black,
-        darkgray: '#18181b',
-        lightgray: '#cbd5e1',
         gray: colors.gray,
         white: colors.white,
         transparent: "transparent",
@@ -31,7 +15,21 @@ export default {
       spacing: {
         18: "4.5rem",
       },
-    }
->>>>>>> 953071aee138c0dc084c011ed7b1a179c75d4c2b
-  },
+    theme: {
+      extend: {
+        colors: {
+          black: colors.black,
+          darkgray: '#18181b',
+          lightgray: '#cbd5e1',
+          gray: colors.gray,
+          white: colors.white,
+          transparent: "transparent",
+          red: colors.red,
+        },
+        spacing: {
+          18: "4.5rem",
+        },
+      },
+    },
+  }
 } as Options;
